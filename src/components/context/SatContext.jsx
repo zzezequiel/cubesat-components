055eb12e905af9770f61af91a58db1c05ff65fb4
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-11-07 17:47:20 +0100

    home styles

---
+++ src/components/context/SatContext.jsx
@@ -0,0 +1,21 @@
+import {createContext, useEffect, useState} from 'react'
+
+
+const SatContext = () => {
+
+useEffect(() => {
+    const data = async () => {
+        
+    }
+}, [])
+
+
+
+
+    
+  return (
+    <div>SatContext</div>
+  )
+}
+
+export default SatContext
